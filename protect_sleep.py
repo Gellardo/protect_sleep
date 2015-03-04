a7820a2b61a255cc71a9ac58a72c27280047d4dd
Frank Schmidt <rubiccuber@googlemail.com>
2015-03-04 16:16:06 +0100

    basic adjustment of deadlines implemented (#4)

--- protect_sleep.py
+++ protect_sleep.py
@@ -5,7 +5,14 @@ import time
 
 ##policies -> per weekday and deadline correction for different programms
 daily_deadlines = {'1': '23:00', '2': '23:00', '3': '23:00', '4': '23:00', '5': '23:00', '6': '23:00', '0': '23:00'}
+modifiers = {'firefox':'00:15','urxvt': '00:05'}
 sleep_minutes = 5
+
+def get_process_list():
+    import subprocess
+    string = subprocess.Popen(['ps','xo', 'comm'], stdout=subprocess.PIPE).communicate()[0]
+    string = string.decode()
+    return string.split('\n')[1:len(string)-3]
 
 def notify_user():
     print("hey man, go to sleep")
@@ -21,6 +28,18 @@ def minutes_to_hours(time):
     input: time in minutes, output: same in 'HH:MM'
     """
     return str(time // 60) + ':' + str(time % 60)
+
+def modify_time(processes, c_t, debug=False):
+    """
+    return the current time c_t warped into the future depending on running processes.
+    """
+    delta_t = 0
+    for p in processes:
+        if p in modifiers:
+            delta_t = max(hours_to_minutes(modifiers[p]), delta_t)
+    if delta_t > 0 and debug:
+        print('added this many minutes:',str(delta_t))
+    return minutes_to_hours(hours_to_minutes(c_t) + delta_t)
 
 def is_deadline_broken(deadlines, c_d, c_t, debug=False):
     """
@@ -57,6 +76,10 @@ if __name__ == '__main__':
         if( daily_deadlines[curr_day] ):
         #if we have a deadline for today
             current_time = time.strftime('%H:%M')
+
+            #look for corrections for open programms
+            processes = get_process_list()
+            current_time = modify_time(processes, current_time)
 
             if( is_deadline_broken(daily_deadlines, curr_day, current_time) ):
                 deadline_broken = True
