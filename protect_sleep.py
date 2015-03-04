9824da653d2d1ddcf8bfc00ee8de4c7e789675bd
Frank Schmidt <rubiccuber@googlemail.com>
2015-03-04 16:15:11 +0100

    refactored hours to minute calcs and added debug messages

--- protect_sleep.py
+++ protect_sleep.py
@@ -10,7 +10,19 @@ sleep_minutes = 5
 def notify_user():
     print("hey man, go to sleep")
 
-def is_deadline_broken(deadlines, c_d, c_t):
+def hours_to_minutes(time):
+    """
+    input: 'HH:MM', output: same time in minutes
+    """
+    time = time.split(':')
+    return int(time[0])*60 + int(time[1])
+def minutes_to_hours(time):
+    """
+    input: time in minutes, output: same in 'HH:MM'
+    """
+    return str(time // 60) + ':' + str(time % 60)
+
+def is_deadline_broken(deadlines, c_d, c_t, debug=False):
     """
     return True if the time c_t lies within a 4 hours time window after
     one of the deadlines of dl_t
@@ -18,18 +30,20 @@ def is_deadline_broken(deadlines, c_d, c_t):
     format of deadlines: { 'D':'HH:MM' }
     format of c_d, c_t: 'D', 'HH:MM'
     """
-    c_t = c_t.split(':')
-    c_t = int(c_t[0])*60 + int(c_t[1])
+    c_t = hours_to_minutes(c_t)
 
     #loop to check the current day and the day before
     for i in [0,1]:
         dl_t = deadlines[str( (int(c_d)-i)%7 )]
-        dl_t = dl_t.split(':')
-        #correct the minute representation of the time depending on the day modification
-        dl_t = int(dl_t[0])*60 + int(dl_t[1]) - i*24*60
+        #correct the minutes representation of the time depending on the day modification
+        dl_t = hours_to_minutes(dl_t) - i*24*60
 
         if( dl_t < c_t and dl_t+4*60 > c_t ):
+            if debug:
+                print('deadline broken')
             return True
+    if debug:
+        print('no deadline broken')
     return False
 
 if __name__ == '__main__':
